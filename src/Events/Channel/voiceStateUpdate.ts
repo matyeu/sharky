cf766f4907e57607bddfca328b5ad33ee61cc738
matyeu <mathieuletutour@gmail.com>
2023-03-04 20:50:07 +0100

    Update file voiceStateUpdate

--- src/Events/Channel/voiceStateUpdate.ts
+++ src/Events/Channel/voiceStateUpdate.ts
@@ -5,7 +5,7 @@ export default async function (client: SharkClient, oldMember: VoiceState, newMe
     
     setInterval(() => {
         if (!newMember.selfVideo && !newMember.streaming) giveCoihs(client, newMember.guild.id, newMember.id, 300);
-        if (newMember.streaming || newMember.streaming) giveCoihs(client, newMember.guild.id, newMember.id, 400);
+        if (newMember.streaming) giveCoihs(client, newMember.guild.id, newMember.id, 400);
         if (newMember.selfVideo) giveCoihs(client, newMember.guild.id, newMember.id, 500);
     }, 900000);
 
